6998ab3a407bc3bf604d6678fd4cfd49a6af2447
Jonatan Lachor <lachorjonatan@icloud.com>
2025-03-01 18:35:00 +0100

    Add stylesheets including fanta styles, add header and tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import Header from './components/Header';
+import Tabs from './components/Tabs';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 const App = () => {
+  const todos = [
+    { input: 'Poszprzatac pokoj', complete: true },
+    { input: 'Zrobić zakupy', complete: true },
+    { input: 'Poćwiczyć', complete: false },
+  ];
 
-  return <div>
-
-  </div>;
+  return (
+    <>
+      <Header todos={todos} />
+      <Tabs todos={todos}/>
+      <TodoList todos={todos}/>
+      <TodoInput />
+    </>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
+import './fanta.css'
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
