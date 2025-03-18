7fbbf7dd1b238f618ac6b22a67b69997d844b763
Jonatan Lachor <lachorjonatan@icloud.com>
2025-03-18 11:09:26 +0100

    Handle task adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import Header from './components/Header';
 import Tabs from './components/Tabs';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 const App = () => {
-  const todos = [
-    { input: 'Poszprzatac pokoj', complete: true },
-    { input: 'Zrobić zakupy', complete: true },
-    { input: 'Poćwiczyć', complete: false },
-  ];
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (newTask) => {
+    if (!newTask) return;
+    const newTodoList = [...todos, { input: newTask, complete: false }];
+    setTodos(newTodoList);
+  };
+
+  const handleEditTodo = () => {};
+
+  const handleDeleteTodo = () => {};
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos} />
-      <TodoInput />
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   );
 };
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,22 @@
-const TodoInput = () => {
-  return (
-    <div>
+import { useState } from 'react';
 
+const TodoInput = (props) => {
+  const { handleAddTodo } = props;
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        placeholder="Add task"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={() => handleAddTodo(inputValue)}>
+        <i className="fa-solid fa-plus"></i>
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default TodoInput;
